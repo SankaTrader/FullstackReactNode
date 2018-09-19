4b648ca197b4d90232e0186e7437013e9d23203a
Mohamed Said <mac@Macs-MacBook-Pro.local>
2018-09-19 14:03:32 -0500

    changed greeting

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
-const app = express('upper-case');
 
+const passport = require('passport');
 
-app.get('/', (req, res) => {
-    res.send({what: 'there up'});
-});
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+const app = express();
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID, 
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+       
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile:', profile);
+    } 
+  )
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+
+        scope: ['profile', 'email']
+
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-//import express from 'express';
 
